5c0a19cbd8e8aecd2224b2979fc09059de294540
unknown <oboreisa@gmail.com>
2021-01-17 21:00:12 +0200

    updating lifecycle hook

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 // ES6 JavaScript
-const person = (props) => {
-    return  (
-    <div>
-        <p onClick={props.click}>I am a {props.name} and I am {props.age} old! </p>
-        <input type="text" onChange={props.changed} value={props.name}/>
-    </div>
-    )
-};
 
-export default person;
+class Person extends Component {
+    // static getDerivedStateFromProps (props, state) {
+    //     console.log('[Person.js] getDerivedStateFrom ');
+    //     return state;
+    // }
+
+    // componentWillReceiveProps(props) {
+    //     console.log('[Person.js] componentWillReceiveProps');
+    // }
+
+    shouldComponentUpdate (nextProps, nextState) {
+        console.log('[Person.js] shouldComponentUpdate');
+        return true; //compare props and state
+    }
+
+    getSnapshotBeforeUpdate (prevProps, prevState) {
+        console.log('[Person.js] getSnapshotBeforeUpdate');
+        return {message : 'Snapshot!'};
+    }
+
+    componentDidUpdate (prevProps, prevState, snapshot) {
+        console.log('[Person.js] componentDidMount');
+        console.log(snapshot);
+    }
+
+    render () {
+        return (
+        <div>
+            <p onClick={this.props.click}>I am a {this.props.name} and I am {this.props.age} old! </p>
+            <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        </div> 
+        )
+    }
+}; 
+
+
+export default Person;
